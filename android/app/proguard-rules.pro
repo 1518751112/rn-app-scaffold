feb393da26169a72812f8c6b7e557ef9bb256023
浮云 <1518751112@qq.com>
2023-10-13 14:28:03 +0800

    增加环境变量配置与打包配置

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -8,3 +8,5 @@
 #   http://developer.android.com/guide/developing/tools/proguard.html
 
 # Add any project specific keep options here:
+#“rn_app”是您在 AndroidManifest.xml 文件中使用的包名称。 配置环境变量
+-keep class com.rn_app.BuildConfig { *; }
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,5 @@
-
+import RNConfig from 'react-native-config'
 export const config = {
-    serverHome: "http://192.168.31.8:15030/"
+    serverHome: "http://192.168.31.8:15030/",
+    CODEPUSH_KEY_IOS: RNConfig.CODEPUSH_KEY_IOS,
 }
